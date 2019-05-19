f984ff7c5cf864cfdd67530b1bf35f5010aecb5c
Thomas Chartron <thomas.chartron@gmail.com>
2019-05-19 16:31:52 +0200

    :sparkles: logging to api works

--- main.js
+++ main.js
@@ -1,9 +1,54 @@
 const fs = require('fs');
+const axios = require('axios');
 require('log-timestamp');
 
 const buttonPressesLogFile = '/home/thomas/dev/javascript/node/';
 
 console.log(`Watching for file changes on ${buttonPressesLogFile}`);
+
+//Axios config
+const remoteAddr = "http://192.168.0.24/api";
+const token = "";
+const loggedAxios = axios.create({
+  baseURL: remoteAddr,
+  timeout: 1000,
+  headers: { "Authorization": "Bearer " + token }
+});
+
+const apiUsername = "thomas.chartron@gmail.com";
+const apiPassword = "thomasthomas";
+
+
+//Try to login to api
+// var params = JSON.stringify({
+//     email: apiUsername,
+//     password: apiPassword
+// });
+// var headers = {
+//     "Content-Type": "application/json"
+// }
+// axios.post(remoteAddr + "/login", {params, headers})
+// .then((res) => {
+//     console.log('logged')
+//     console.log(res.data.representation);
+// }, (error) => {
+//     console.log(error)
+// });
+
+axios({
+  method: 'post',
+  url: remoteAddr + '/login',
+  headers: {
+    "Content-Type": "application/json"
+  },
+  data: {
+    email: apiUsername,
+    password: apiPassword,
+  }
+}).then(function (response) {
+    console.log('logged')
+    console.log(response.data.access_token);
+});;
 
 // fs.watchFile(buttonPressesLogFile, (curr, prev) => {
 //   console.log(`${buttonPressesLogFile} file Changed`);
