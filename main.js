c60093a084fc2a900770edacf821a043f0dafb52
Thomas Chartron <thomas.chartron@gmail.com>
2019-05-19 16:06:00 +0200

    :sparkles: Improved script with md5 check sum and 100ms delay to emmit change event

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
 const fs = require('fs');
 require('log-timestamp');
 
-const buttonPressesLogFile = '/home/thomas/dev/javascript/node/test.log';
+const buttonPressesLogFile = '/home/thomas/dev/javascript/node/';
 
 console.log(`Watching for file changes on ${buttonPressesLogFile}`);
 
-fs.watchFile(buttonPressesLogFile, (curr, prev) => {
-  console.log(`${buttonPressesLogFile} file Changed`);
+// fs.watchFile(buttonPressesLogFile, (curr, prev) => {
+//   console.log(`${buttonPressesLogFile} file Changed`);
+// });
+
+//MD5 check for real file change not only saves
+// 100MS delay to emmit change event or it fires to often
+let md5Previous = null;
+let fsWait = false;
+fs.watch(buttonPressesLogFile, (event, filename) => {
+  if (filename) {
+    if (fsWait) return;
+    fsWait = setTimeout(() => {
+      fsWait = false;
+    }, 100);
+    const md5Current = md5(fs.readFileSync(buttonPressesLogFile));
+    if (md5Current === md5Previous) {
+      return;
+    }
+    md5Previous = md5Current;
+    console.log(`${filename} file Changed`);
+  }
 });
